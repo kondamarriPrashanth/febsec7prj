7049e1dfa5d08961e8829886647444d2a3c5eba1
abc <abc@gmail.com>
2024-02-26 13:48:28 +0530

    first project

---
+++ Febsec7Prj/Febsec7Prj/Bank.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Febsec7Prj
+{
+    public class Bank
+    {
+        public string Authenticate(string UserName, string UserId)
+        {
+            string msg;
+            if (string.IsNullOrEmpty(UserName) || string.IsNullOrEmpty(UserId))
+            {
+                msg = "user name and password required ";
+            }
+            else
+            {
+                if ((UserName == "Ram") && (UserId == "ram1234"))
+                {
+                    msg = "Authentication pass";
+                }
+                else
+                {
+                    msg = "Authentcation fail";
+                }
+            }
+            return "provide user details";
+
+        }
+    }
+}
+
---
+++ Febsec7Prj/Febsec7Prj/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Febsec7Prj;
+using System.Reflection.Emit;
+using System.Xml.Linq;
+using System;
+using System.Text;
+
+Console.WriteLine("BANK LOGIN PAGE");
+Console.WriteLine("Enter the UserName:");
+string UserName=Console.ReadLine();
+Console.WriteLine("Enter the UserId:");
+int ACCNO=int.Parse(Console.ReadLine());
+Bank b= new Bank();
+Console.WriteLine(b.Authenticate("Ram", "ram123"));
+
